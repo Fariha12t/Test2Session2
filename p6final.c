4138ca3508ae997ecced1513463f7c331b4c097c
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-03-06 14:07:58 +0000

    prime

--- p6final.c
+++ p6final.c
@@ -1,1 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+int string_length(char *s)
+{
+  int i;
+  for(i=0;s[i]!='\0';i++);
+  return i;
+}
+
+int string_ncmp(char *s1,char *s2)
+{
+  int n,i;
+  for(int i=0; i<n && s1[i]==s2[i] && s1[i] != '\0';i++);
+  return s1[i] - s2[i];
+}
+
+
+int string_index(char *s, char *subs)
+{
+  int l1=string_length(s);
+  int l2=string_length(subs);
+  if( l1 < l2)
+      return -1;
+  for(int i=0;i<l1-l2;i++)
+    if (l2>l1)
+      return i;
+  return -1;
+}
+int main()
+{
+  int n;
+  char s1,s2,*s,subs,i;
+  n=string_length(s);
+  string_ncmp(&s1,&s2);
+  string_index(s,&subs);
+  
+  
+  
+  
+  
+  
+}
 
